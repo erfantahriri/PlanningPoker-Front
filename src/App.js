8a109e2bcc40dd3f82b1f8e839fb501f01d3ff39
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-13 20:14:43 +0430

    Move main page sections to Main component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,11 @@
-import './App.css';
-import Button from '@material-ui/core/Button';
-import CreateRoom from './components/CreateRoom';
-import JoinRoom from './components/JoinRoom';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Main from './components/Main';
 
 export class App extends Component {
 
-  state = {
-    "action": undefined
-  }
-
-  createRoomOnClick = (event) => {
-    this.setState({ "action": "create" });
-  }
-
-  joinRoomOnClick = (event) => {
-    this.setState({ "action": "join" });
-  }
-
-  getActionComponent = () => {
-    if (this.state.action === "create") {
-      return (<CreateRoom/>);
-    } else if (this.state.action === "join") {
-      return (<JoinRoom/>);
-    } else {
-      return null;
-    }
-  }
-
   render() {
     return (
-      <div className="App">
-      <header className="App-header">
-        <p>Planning Poker</p>
-        <div>
-          <Button
-            variant="contained"
-            color="primary" 
-            style={{marginRight: "20px"}}
-            onClick={this.joinRoomOnClick}
-          >
-            Join a Room
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary" 
-            style={{marginRight: "20px"}}
-            onClick={this.createRoomOnClick}
-          >
-          Create a Room 
-          </Button>
-        </div>
-        {this.getActionComponent()}
-      </header>
-    </div>
+      <Main />
     )
   }
 }
