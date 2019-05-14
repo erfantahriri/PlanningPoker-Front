9739d88673937b5de47c741955ad4c9ecbc2ea44
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-14 13:25:55 +0430

    Add CreateIssue component and handle flow to create new issue for room

---
+++ src/components/CreateIssue.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default class CreateIssue extends React.Component {
+	render() {
+		return (
+			<div>
+				<Dialog
+					open={this.props.open}
+					onClose={this.handleClose}
+					aria-labelledby="form-dialog-title"
+				>
+					<DialogTitle id="form-dialog-title">Create Issue</DialogTitle>
+					<DialogContent>
+						{/* <DialogContentText>
+							Create an issue.
+            </DialogContentText> */}
+						<TextField
+							autoFocus
+							margin="dense"
+							id="issueTitle"
+							label="Issue Title"
+							type="text"
+							fullWidth
+							onChange={this.props.handleIssueTitleInputChange} 
+						/>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={this.props.handleClose} color="primary">
+							Cancel
+            </Button>
+						<Button onClick={this.props.handleCreateIssue} color="primary">
+							Create
+            </Button>
+					</DialogActions>
+				</Dialog>
+			</div>
+		);
+	}
+}
