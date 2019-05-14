65453a61d0f5f36559a3ae03e107ad7b3edabdc1
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-14 13:24:01 +0430

    Add API call to CreateRoom component and handle response

--- src/services/api.js
+++ src/services/api.js
@@ -1,29 +1,35 @@
 const baseUrl = "http://127.0.0.1:8000"
 
 const urls = {
-    getRoom: baseUrl + "/v1/rooms/",
-    createRoom: baseUrl + "/v1/rooms/",
-    // joinRoom: baseUrl + `/v1/rooms/${room_uid}/join`,
+	getRoom: baseUrl + "/v1/rooms/",
+	createRoom: baseUrl + "/v1/rooms/",
+	getRoomIssues: baseUrl + "/v1/rooms/roomUid/issues",
+	getRoomParticipants: baseUrl + "/v1/rooms/roomUid/participants",
+	createIssue: baseUrl + "/v1/rooms/roomUid/issues",
 }
 
 export const getRooms = () => {
-    return fetch(urls.getRoom)
-    .then(response => response.json())
-    .then(json => console.log(json))
+	return fetch(urls.getRoom)
+		.then(response => response.json())
+		.then(json => console.log(json))
 }
 
 export const createRoom = (title, description, creator_name) => {
-    return fetch(urls.createRoom, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            title: title,
-            description: description,
-            creator_name: creator_name
-        })
-    })
-    .then(response => response.json())
-    .then(json => console.log(json))
+	return fetch(urls.createRoom, {
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			title: title,
+			description: description,
+			creator_name: creator_name
+		})
+	})
+		.then(response => {
+			if (response.status === 201) {
+				return response.json();
+			}
+		})
+		.catch(err => console.log(err))
 }
